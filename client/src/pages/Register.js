3d8a1e99a6ba3b6cec465c5325a31b840d8b93c0
Owen Lenihan <owenlenihan101@gmail.com>
2023-10-08 23:11:00 +0100

    Update Register.js

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,5 +2,19 @@ import { useState, useEffect } from "react";
 import { registerUser } from "../api/serverAuth";
 
 const Register = ({ isAutherized }) => {
-  
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [authData, setAuthData] = useState({});
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    if (isReady) {
+      registerUser(formData).then((data) => setAuthData(data));
+    }
+  })
 }
