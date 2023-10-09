d096d0be82ed5a2cd9f13164e1e6b0496312bf65
Owen Lenihan <owenlenihan101@gmail.com>
2023-10-09 23:53:07 +0100

    Update Register.js

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -16,5 +16,63 @@ const Register = ({ isAutherized }) => {
     if (isReady) {
       registerUser(formData).then((data) => setAuthData(data));
     }
-  })
+  }, [isReady, formData, isAutherized]);
+
+  const { name, email, password, confirmPassword } = formData
+
+  const onChange = (e) => {
+    setFormData({ ...formData, })
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+    } else {
+      setIsReady(true);
+      window.location.reload();
+    }
+  };
+
+  return (
+    <div>
+      <h2>Registration</h2>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type=""
+            name=""
+            id=""
+            value={name}
+            placeholder=""
+            onChange={onChange}
+          />
+          <input
+            type=""
+            name=""
+            id=""
+            value={email}
+            placeholder=""
+            onChange={onChange}
+          />
+          <input
+            type=""
+            name=""
+            id=""
+            value={password}
+            placeholder=""
+            onChange={onChange}
+          />
+          <input
+            type=""
+            name=""
+            id=""
+            value={confirmPassword}
+            placeholder=""
+            onChange={onChange}
+          />
+        </div>
+      </form>
+    </div>
+  )
 }
