9c0d308d86dffed2d1d243a32d015513baa305bd
Owen Lenihan <owenlenihan101@gmail.com>
2023-10-13 23:56:54 +0100

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-
+  email: { type: String, required: true, unique: true },
 })
 
 UserSchema.virtual("url").get(function () {
